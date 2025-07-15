b0c7a4e588291e69ff7f5a2ab34b0063e4384d2e
Gevorg Arzakanyan <zokavski@gmail.com>
2025-07-15 19:43:09 +0400

    done

--- main.js
+++ main.js
@@ -1,28 +1,35 @@
 import { increment } from "./counter.js";
 import { reset } from "./counter.js";
 
+let inc = document.getElementById("increment");
+let disp = document.getElementById("display");
+let res = document.getElementById("reset");
 
-let inc = document.getElementById("increment")
-let disp = document.getElementById("display")
-let res = document.getElementById("reset")
+let sub = document.getElementById("subButton");
+let txtinp = document.getElementById("input");
+let txtout = document.getElementById("output");
 
-let sub = document.getElementById("subButton")
-let txtinp = document.getElementById("form")
-let txtout = document.getElementById("output")
-
+txtinp.addEventListener("keydown", (e) => {
+  console.log(e.key === "Escape");
+  e.key === "Escape" && txtinp.blur();
+});
 
 inc.addEventListener("click", () => {
-    disp.textContent = increment()
+  disp.textContent = increment();
 });
 
 res.addEventListener("click", () => {
-    disp.textContent = reset()
+  disp.textContent = reset();
 });
 
 sub.addEventListener("click", () => {
-    let count = txtinp.value.length;
-    if (count > 2) {
-        txtout.textContent = "Wlecome"
-        txtinp.value = "";
-    } else { txtout.textContent = "Not Wlecome"}
+  let counter = txtinp.value.length;
+  if (counter > 2) {
+    txtout.style.color = "black";
+    txtout.textContent = "Welcome";
+    txtinp.value = "";
+  } else {
+    txtout.textContent = "Erorr: input should be at least 3 chars";
+    txtout.style.color = "red";
+  }
 });
